54f3f43906d66a568ec33322f2cf36203c3e8ef3
陈煜晴 <ccccyq0329@mail.ustc.edu.cn>
2023-12-16 11:00:57 +0800

    Update land.py

--- land.py
+++ land.py
@@ -3,30 +3,33 @@ from pygame import *
 class Land:
     def __init__(self, index): #index为类型序号，0河流 1河边草地 2平原草地 3平原森林 4高山草地 5高山荒地
         self.index = index
+        self.qualt_H2O = 0
+        self.qualt_resc = 0
+        self.qualt_metbls = 0
         match self.index:
-            case "0":
+            case 0:
                 self.land_type = "river"
-            case "1":
+            case 1:
                 self.land_type = "riverside_meadow" #类型名称
                 self.qualt_H2O = 1.5 #水量因子
                 self.qualt_resc = 1 #资源量因子
                 self.qualt_metbls = 1.2 #代谢率因子
-            case "2":
+            case 2:
                 self.land_type = "plain_meadow"
                 self.qualt_H2O = 1
                 self.qualt_resc = 1
                 self.qualt_metbls = 1
-            case "3":
+            case 3:
                 self.land_type = "plain_forest"
                 self.qualt_H2O = 1
                 self.qualt_resc = 1.5
                 self.qualt_metbls = 1.2
-            case "4":
+            case 4:
                 self.land_type = "mountain_meadow"
                 self.qualt_H2O = 0.8
                 self.qualt_resc = 0.8
                 self.qualt_metbls = 0.6
-            case "5":
+            case 5:
                 self.land_type = "mountain_desert"
                 self.qualt_H2O = 0.6
                 self.qualt_resc = 0.1
