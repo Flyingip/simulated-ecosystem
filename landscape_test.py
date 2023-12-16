c5aac407c7b9ae9ac5dc2999d5f3efd6866e96c7
陈煜晴 <ccccyq0329@mail.ustc.edu.cn>
2023-12-16 11:00:59 +0800

    Update landscape_test.py

--- landscape_test.py
+++ landscape_test.py
@@ -19,9 +19,26 @@ game_display.blit(land_img, (0, 0))
 
 #地图数据
 landscape = [[[] * 4] * 800] * 800
+Land_map = [[Land(0) for _ in range(800)] for _ in range(800)]
 for i in range(0, 800):
     for j in range(0, 800):
         landscape[i][j] = game_display.get_at((i, j))
+        if landscape[i][j] == (1, 173, 255, 255):
+            Land_map[i][j] = Land(0)
+        elif landscape[i][j] == (24, 173, 1, 255):
+            Land_map[i][j] = Land(1)
+        elif landscape[i][j] == (36, 255, 1, 255):
+            Land_map[i][j] = Land(2)
+        elif landscape[i][j] == (12, 86, 1, 255):
+            Land_map[i][j] = Land(3)
+        elif landscape[i][j] == (231, 255, 1, 255):
+            Land_map[i][j] = Land(4)
+        elif landscape[i][j] == (120, 132, 1, 255):
+            Land_map[i][j] = Land(5)
+        else:
+            Land_map[i][j] = Land(2)
+        print(Land_map[i][j].qualt_H2O, end=' ')
+            
 
 crashed=False
 
