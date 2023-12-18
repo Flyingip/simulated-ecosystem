5993cc8423b1dd5d1039426517db05a01d05838a
陈煜晴 <ccccyq0329@mail.ustc.edu.cn>
2023-12-18 17:39:01 +0800

    Update landscape_test.py

--- landscape_test.py
+++ landscape_test.py
@@ -1,49 +1,61 @@
 from pygame import *
 from land import *
-# init()
+import matplotlib.pyplot as plt
 
-clock=time.Clock()
+def land_comp(rgb):
+    r = [(1, 173, 255, 255),
+         (24, 173, 1, 255),
+         (36, 255, 1, 255),
+         (12, 86, 1, 255),
+         (231, 255, 1, 255),
+         (120, 132, 1, 255)]
+    if rgb == (1, 173, 255, 255):
+        type = 0
+    elif rgb == (24, 173, 1, 255):
+        type = 1
+    elif rgb == (36, 255, 1, 255):
+        type = 2
+    elif rgb == (12, 86, 1, 255):
+        type = 3
+    elif rgb == (231, 255, 1, 255):
+        type = 4
+    elif rgb == (120, 132, 1, 255):
+        type = 5
+    else:
+        err = [0, 0, 0, 0, 0, 0]
+        for i in range(6):
+            for j in range(2):
+                err[i] = (rgb[j] - r[i][j]) ** 2 + err[i]
+        min_err = min(err)
+        type = err.index(min_err)
+    return type
 
-# 地图的大小
+def land_plot(landscape_index):
+    color_map = np.zeros((800, 800, 4), dtype=np.uint8)
+    colors = [(1, 173, 255, 255),
+              (24, 173, 1, 255),
+              (36, 255, 1, 255),
+              (12, 86, 1, 255),
+              (231, 255, 1, 255),
+              (120, 132, 1, 255)]
+    for i in range(0, 800):
+        for j in range(0, 800):
+            color_map[i, j, :] = colors[int(landscape_index[j, i])]
+    plt.imshow(color_map)
+    plt.axis('off')
+    plt.show()
 
-h=800
-w=800
-
-#地图
-
-game_display=display.set_mode((h,w))
-display.set_caption("simulated-ecosystem")
-
-land_img = image.load("Land_v1_1.png")
-game_display.blit(land_img, (0, 0))
-
-#地图数据 初始化
-landscape = [[[] * 4] * 800] * 800
-Land_map = [[Land(0) for _ in range(800)] for _ in range(800)]
-for i in range(0, 800):
-    for j in range(0, 800):
-        landscape[i][j] = game_display.get_at((i, j))
-        if landscape[i][j] == (1, 173, 255, 255):
-            Land_map[i][j] = Land(0)
-        elif landscape[i][j] == (24, 173, 1, 255):
-            Land_map[i][j] = Land(1)
-        elif landscape[i][j] == (36, 255, 1, 255):
-            Land_map[i][j] = Land(2)
-        elif landscape[i][j] == (12, 86, 1, 255):
-            Land_map[i][j] = Land(3)
-        elif landscape[i][j] == (231, 255, 1, 255):
-            Land_map[i][j] = Land(4)
-        elif landscape[i][j] == (120, 132, 1, 255):
-            Land_map[i][j] = Land(5)
-        else:
-            Land_map[i][j] = Land(2)
-            
-
-crashed=False
-
-number_of_days = 5000
-
-# world
-
-for day in range(0,number_of_days):
-    display.flip()
+def water_plot(landscape_index):
+    color_map = np.zeros((800, 800, 4), dtype=np.uint8)
+    colors = [(1, 173, 255, 255),
+              (24, 173, 1, 255),
+              (36, 255, 1, 255),
+              (12, 86, 1, 255),
+              (231, 255, 1, 255),
+              (120, 132, 1, 255)]
+    for i in range(0, 800):
+        for j in range(0, 800):
+            color_map[i, j, :] = colors[int(landscape_index[j, i])]
+    plt.imshow(color_map)
+    plt.axis('off')
+    plt.show()
