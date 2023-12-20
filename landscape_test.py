6d2440db623814a2bb006d91202a7feafb5e0c1f
陈煜晴 <ccccyq0329@mail.ustc.edu.cn>
2023-12-20 17:01:11 +0800

    Update landscape_test.py

--- landscape_test.py
+++ landscape_test.py
@@ -1,6 +1,8 @@
 from pygame import *
 from land import *
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
 def land_comp(rgb):
     r = [(1, 173, 255, 255),
@@ -22,8 +24,8 @@ def land_comp(rgb):
     elif rgb == (120, 132, 1, 255):
         type = 5
     else:
-        err = [0, 0, 0, 0, 0, 0]
-        for i in range(6):
+        err = [0, 0, 0, 0, 0]
+        for i in range(5):
             for j in range(2):
                 err[i] = (rgb[j] - r[i][j]) ** 2 + err[i]
         min_err = min(err)
@@ -40,22 +42,14 @@ def land_plot(landscape_index):
               (120, 132, 1, 255)]
     for i in range(0, 800):
         for j in range(0, 800):
-            color_map[i, j, :] = colors[int(landscape_index[j, i])]
+            color_map[i, j, :] = colors[int(landscape_index[i, j])]
     plt.imshow(color_map)
     plt.axis('off')
     plt.show()
 
-def water_plot(landscape_index):
-    color_map = np.zeros((800, 800, 4), dtype=np.uint8)
-    colors = [(1, 173, 255, 255),
-              (24, 173, 1, 255),
-              (36, 255, 1, 255),
-              (12, 86, 1, 255),
-              (231, 255, 1, 255),
-              (120, 132, 1, 255)]
-    for i in range(0, 800):
-        for j in range(0, 800):
-            color_map[i, j, :] = colors[int(landscape_index[j, i])]
-    plt.imshow(color_map)
-    plt.axis('off')
+def water_plot(land_index):
+    df = pd.DataFrame(land_index)
+    sns.heatmap(df, vmin=0, vmax=18, cmap="Blues", annot=False, xticklabels=False, yticklabels=False)  
     plt.show()
+    plt.pause(0.001)
+    plt.clf()
