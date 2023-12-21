a78461e10580b3a1fa166231dd06cf3625cbccd9
陈煜晴 <ccccyq0329@mail.ustc.edu.cn>
2023-12-21 13:21:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ Land_map = [[Land(0) for _ in range(800)] for _ in range(800)]
 landscape_index = np.zeros((800, 800))
 water_index = np.zeros((800, 800))
 resc_prodc = np.zeros((800, 800))
+river=[]#水源地块
 for i in range(0, 800):
     for j in range(0, 800):
         landscape[i][j] = game_display.get_at((j, i))  # 获取像素点颜色
@@ -36,8 +37,11 @@ for i in range(0, 800):
         landscape_index[i][j] = Land_map[i][j].index  # 获取索引用于画图
         water_index[i][j] = Land_map[i][j].orig_soil_H2O # 地块含水量的二维地图
         resc_prodc[i][j] = Land_map[i][j].orig_plant_C # 地块植物碳的二维地图
+        if Land_map[i][j].index == 0:
+            river.append([i, j])
 #land_plot(landscape_index)  # plot land
 #water_plot(water_index)
+#print(river)
 
 # 生物
 
@@ -45,16 +49,15 @@ for i in range(0, 800):
 wolf=[]
 rabbit=[]
 wolf_1=biology(2,4,1,10,5,100,100,[2,1],rabbit,'none',wolf)
-rabbit_1=biology(3,5,2.5,10,5,100,100,[5,60],'grass',wolf,rabbit)#分别代表生物的运动能力，感知范围，年龄,寿命,幼年体重,初始体重，初始储存能量,初始位置，食物,天敌,族群
+rabbit_1=biology(3,5,2.5,10,5,100,100,[459, 416],'grass',wolf,rabbit)#分别代表生物的运动能力，感知范围，年龄,寿命,幼年体重,初始体重，初始储存能量,初始位置，食物,天敌,族群
 rabbit_2=biology(3,5,2.5,10,5,100,100,[5,59],'grass',wolf,rabbit)
 rabbit_2.partner_is=1
 wolf.append(wolf_1)
 rabbit.append(rabbit_1)
 rabbit.append(rabbit_2)
-water=[]#水源地块
-for i in range(5,10):
+'''for i in range(5,10):
     for j in range(10,20):
-        water.append([i,j])
+        river.append([i,j])'''
 rabbit_1.memory_data=[[7,15]]#初始记忆地块
 
 crashed=False
@@ -127,13 +130,13 @@ for day in range(number_of_days):
             rabbit_1.bear_dec()
         elif (rabbit_1.bear<25)&(rabbit_1.enermy_mem.bear<25):#如果双方都没有耐力值剩余，两者以最大速度展开追逐
             rabbit_1.no_is=rabbit_1.escape(rabbit_1.enermy_mem,rabbit_1.speed,rabbit_1.enermy_mem.speed)
-        rabbit_1.memory(water)#记忆可能遇到的水源
+        rabbit_1.memory(river)#记忆可能遇到的水源
         print(rabbit_1.no_is,rabbit_1.energy_lose,rabbit_1.energystorage,rabbit_1.setting,rabbit_1.memory_data)
     elif rabbit_1.energystorage<=50:#如果储存的能量不足，开始觅食运动
         rabbit_1.hungry_is=1
         rabbit_1.walk()#随机运动
-        rabbit_1.memory(water)#记忆可能遇到的水源
-        rabbit_1.eatgrass(resc_prodc)#获取地块上的全部能量
+        rabbit_1.memory(river)#记忆可能遇到的水源
+        rabbit_1.eatgrass(Land_map)#获取地块上的全部能量**********
         print(rabbit_1.setting,rabbit_1.energystorage,rabbit_1.energy_lose,rabbit_1.memory_data)
     elif rabbit_1.thirs<=30:#如果水分不足，开始觅水
         target=rabbit_1.seekforwater()
@@ -141,7 +144,7 @@ for day in range(number_of_days):
         print(rabbit_1.setting,target,rabbit_1.thirs)
     elif rabbit_1.partner_is==1:#如果处于发情期，开始寻找配偶
         rabbit_1.walk()#随机运动
-        rabbit_1.memory(water)#记忆可能遇到的水源
+        rabbit_1.memory(river)#记忆可能遇到的水源
         rabbit_1.part_is,rabbit_1.part_mem=rabbit_1.seeforpart_mem(rabbit)#寻找周围环境中可能存在的配偶
         rabbit_1.sex(rabbit_1.part_mem)
         print(rabbit_1.part_is,rabbit_1.part_mem.setting,rabbit_1.setting,rabbit_1.baby_is)
@@ -153,8 +156,9 @@ for day in range(number_of_days):
         rabbit_1.no_is=0
     if rabbit_1.energystorage<=0:
         rabbit_1.no_is=0
-    if rabbit_1.no_is==0:
-        rabbit.remove(rabbit_1)
+    '''if rabbit_1.no_is==0:
+        rabbit.remove(rabbit_1)'''
+    rabbit_1.thirs-=25
 
 
 
